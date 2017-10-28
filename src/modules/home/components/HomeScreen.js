f4eaf26f03e634451a25d4c85939934a81e6c55d
RAFAEL-NOTEBOOK\Rafael <rafaelsmag@gmail.com>
2017-10-27 23:02:28 -0200

    Changig the color

--- src/modules/home/components/HomeScreen.js
+++ src/modules/home/components/HomeScreen.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-const HomeScreen = () => (
-  <View>
-    <Text>
-      Olá mundo!
-    </Text>
-  </View>
-);
+class HomeScreen extends Component {
+
+  static navigationOptions = {
+    title: 'CrossFit',
+    headerTitleStyle: {
+      color: '#f5f5f5'
+    },
+    headerStyle: {
+      backgroundColor: '#3f3f3f'
+    },
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>
+          Olá mundo do CrossFit!
+        </Text>
+      </View>
+    );
+  }
+}
 
 export default HomeScreen;
