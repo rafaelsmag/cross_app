f86ac9150f7cc0fc9715fa38724d51c96d708c11
Bruno Talhate <btalhate@gmail.com>
2017-11-15 21:35:44 -0200

    Added MovementsItem

--- src/modules/movementsList/styles/index.js
+++ src/modules/movementsList/styles/index.js
@@ -1,28 +1,14 @@
 /* eslint-disable import/prefer-default-export */
 import { StyleSheet } from 'react-native';
 
+const SPACING = 4;
 
 export const MovementsListStyle = StyleSheet.create({
   listContainer: {
     paddingBottom: 20,
-  },
-  itemContainer: {
-    flex: 1,
-    height: 200,
-    padding: 5,
-  },
-  itemImageBackground: {
-    width: '100%',
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'flex-end',
-  },
-  itemTitleContainer: {
-    backgroundColor: '#ffffff55',
-  },
-  itemTitle: {
-    fontSize: 18,
-    color: 'black',
+    paddingTop: SPACING / 2,
+    paddingHorizontal: SPACING / 2,
+    backgroundColor: '#eeeeee',
   },
   headerTitle: {
     color: '#f5f5f5',
@@ -30,4 +16,32 @@ export const MovementsListStyle = StyleSheet.create({
   header: {
     backgroundColor: '#3f3f3f',
   }
+});
+
+export const MovementsItemStyle = StyleSheet.create({
+  itemContainer: {
+    aspectRatio: 1,
+    flex: 1,
+    margin: SPACING / 2,
+  },
+  itemImageBackground: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+  },
+  itemToast: {
+    backgroundColor: 'rgba(0,0,0,0.3)',
+    flexDirection: 'row',
+    alignItems: 'center',
+    minHeight: 48,
+  },
+  itemTitleContainer: {
+    paddingHorizontal: 16,
+  },
+  itemTitle: {
+    // flex: 1,
+    fontSize: 16,
+    color: 'white',
+    fontWeight: '600',
+  },
 });
